54ceefa42dca65d6bbd15a7bd9c1e6b6ce9b774f
Panda Rajaram <prajaram@miteksystems.com>
2021-04-27 14:36:57 -0700

    Bug test

--- ObjectiveC CodeQL Test/ViewController.m
+++ ObjectiveC CodeQL Test/ViewController.m
@@ -16,6 +16,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self ultimateBug];
+}
+
+- (void)ultimateBug {
+    [self ultimateBug];
 }
 
 
